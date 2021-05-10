11cd525a8592ba534ee1896114dc5786ddc8ba89
PranayTyagi1 <>
2021-05-10 16:29:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('button').addEventListener('click', function () {
+  let num1 = document.querySelector('.first-num').value
+  let num2 = document.querySelector('.second-num').value
+  let answer = 0
+  num1 = parseInt(num1)
+  num2 = parseInt(num2)
+  for (counter = 0; counter < num1; counter ++) {
+    answer = answer + num2
+  }
+  alert(answer)
+})
